1397fa5aa288acc5790dfc557a8964e85d33321b
Paliuc Filip <filip.paliuc@student.upt.ro>
2021-05-09 01:42:24 +0300

    Added Tags enum

---
+++ src/app/_utility/tag.enum.ts
@@ -0,0 +1,14 @@
+export enum enTag{
+    Default,
+    Trending,
+    Action,
+	Comedy,
+	Romantic,
+	Adventure,
+	Musical,
+	Drama,
+	Animated,
+	Horror,
+	SciFi,
+	War
+}
--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,8 +1,10 @@
+import { enTag } from './../_utility/tag.enum';
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Movie } from '../_model/movie.model';
 import { Tag } from '../_model/tag.model';
 import { MovieService } from '../_services/movie.service';
+
 
 @Component({
   selector: 'app-homepage',
@@ -12,13 +14,22 @@ import { MovieService } from '../_services/movie.service';
 export class HomepageComponent implements OnInit {
 
   movies: Movie[];
-  tags: Tag[];
+  tags: any;
 
   constructor(
     private movieService: MovieService,
   ) { }
 
+  StringIsNumber = value => isNaN(Number(value)) === false;
+
+  ToArray(ennume) {
+    return Object.keys(ennume)
+        .filter(this.StringIsNumber)
+        .map(key => ennume[key]);
+  }
+
   ngOnInit(): void {
+    this.tags = this.ToArray(enTag);
   }
 
   getAllMovies(){
@@ -36,20 +47,20 @@ export class HomepageComponent implements OnInit {
 
   }
 
-  getMoviesByTags(){
+  // getMoviesByTags(){
 
-    this.tags = new Array(new Tag(1,"Trending"));
-    this.movieService.getMoviesByTags(this.tags).subscribe(
-      (response: Movie[]) => {
-        this.movies = response;
-        console.log(response);
-      },
+  //   this.tags = new Array(new Tag(1,"Trending"));
+  //   this.movieService.getMoviesByTags(this.tags).subscribe(
+  //     (response: Movie[]) => {
+  //       this.movies = response;
+  //       console.log(response);
+  //     },
 
-      (error: HttpErrorResponse) => {
-        console.log(error);
-      }
-    )
+  //     (error: HttpErrorResponse) => {
+  //       console.log(error);
+  //     }
+  //   )
 
-  }
+  // }
 
 }
