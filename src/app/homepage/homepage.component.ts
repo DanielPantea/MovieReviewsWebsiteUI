eb1b15bcee51d189c190cea4bb80384789e3e136
Paliuc Filip <filip.paliuc@student.upt.ro>
2021-05-09 01:16:54 +0300

    Tested on Homepage

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,4 +1,8 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Movie } from '../_model/movie.model';
+import { Tag } from '../_model/tag.model';
+import { MovieService } from '../_services/movie.service';
 
 @Component({
   selector: 'app-homepage',
@@ -7,9 +11,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomepageComponent implements OnInit {
 
-  constructor() { }
+  movies: Movie[];
+  tags: Tag[];
+
+  constructor(
+    private movieService: MovieService,
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  getAllMovies(){
+
+    this.movieService.getAllMovies().subscribe(
+      (response: Movie[]) => {
+        this.movies = response;
+        console.log(response);
+      },
+
+      (error: HttpErrorResponse) => {
+        console.log(error);
+      }
+    )
+
+  }
+
+  getMoviesByTags(){
+
+    this.tags = new Array(new Tag(1,"Trending"));
+    this.movieService.getMoviesByTags(this.tags).subscribe(
+      (response: Movie[]) => {
+        this.movies = response;
+        console.log(response);
+      },
+
+      (error: HttpErrorResponse) => {
+        console.log(error);
+      }
+    )
+
   }
 
 }
